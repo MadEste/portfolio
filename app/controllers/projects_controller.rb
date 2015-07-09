cf0bddff8142ab52015ccf624f7d987e79bdfecf
Este <estevansyfried@gmail.com>
2015-07-08 18:15:25 -0700

    Added Categories and association with Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,43 @@
 class ProjectsController < ApplicationController
+	before_action :find_project, only:[:show,:edit,:update,:destroy]
+	
 	def index
+	end
+
+	def show
+
+	end
+
+	def new 
+		@project = Project.new
+	end
+
+	def create 
+		@project = Project.new(project_params)
+		if @project.save
+			redirect_to @project
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_project
+		@project = Project.find(params[:id])
+	end
+
+	def project_params
+		params.require(:project).permit( :title, :description, :url)
 	end
 	
 end
